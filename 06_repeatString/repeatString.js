824b9d22c2fb432bea232667a8a9f2aa44f485a5
Jordan-AM <163789391+Jordan-AM@users.noreply.github.com.>
2025-07-21 18:23:25 -0300

    Solve 06_repeatString
    Use if statement and loop

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  result = "";
+  if (num >= 0) {
+      for (let i = 0; i < num; i++) {
+        for (const letter of word) {
+          result += letter;
+        }
+      }
+  } else {
+    result = "ERROR"
+  }
+  return result;
 };
 
 // Do not edit below this line
