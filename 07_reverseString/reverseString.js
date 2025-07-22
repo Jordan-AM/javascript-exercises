dc805e7d9e93049a9e1cc02a99546b9caafe6add
Jordan-AM <163789391+Jordan-AM@users.noreply.github.com.>
2025-07-21 21:42:46 -0300

    Solve 07_reverseString
    Do a negative loop, decreasing the index in the array
    instead of increasing it like normal

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (array) {
+  let result = ``;
+  for (let index = array.length; index > 0; index--) {
+    result += array[index - 1];
+  }
+  return result;
 };
 
 // Do not edit below this line
