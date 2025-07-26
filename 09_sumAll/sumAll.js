7e0877445c43449df0e92bedab1506f904db2b43
Jordan-AM <163789391+Jordan-AM@users.noreply.github.com.>
2025-07-26 13:16:46 -0300

    Solve sumAll
    Lump all ERROR in the same if statement
    Ensure that:
    the lower number is at the start with Math.min
    and the higher is at the end with Math.max

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+function sumAll(start, end) {
+  // Error handling upfront
+  if (
+    typeof start !== "number" ||
+    typeof end !== "number" ||
+    start < 0 ||
+    end < 0 ||
+    !Number.isInteger(start) ||
+    !Number.isInteger(end)
+  ) {
+    return "ERROR";
+  }
 
-};
+  // Ensure start is less than end
+  const min = Math.min(start, end);
+  const max = Math.max(start, end);
+
+  let total = 0;
+  for (let i = min; i <= max; i++) {
+    total += i;
+  }
+
+  return total;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
