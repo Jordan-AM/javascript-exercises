9c44e0b4482e1ac5838b4f6544dde40a03c3ea97
Jordan-AM <163789391+Jordan-AM@users.noreply.github.com.>
2025-07-26 15:03:34 -0300

    Solve leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+function leapYears(year) {
+  if (year % 400 === 0) {
+    return true; // divisible by 400: leap year
+  }
+  if (year % 100 === 0) {
+    return false; // divisible by 100 but not 400: not a leap year
+  }
+  if (year % 4 === 0) {
+    return true; // divisible by 4: leap year
+  }
+  return false; // not divisible by 4: not a leap year
+}
 
-};
 
 // Do not edit below this line
 module.exports = leapYears;
